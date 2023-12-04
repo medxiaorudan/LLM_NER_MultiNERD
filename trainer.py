25fc0d29978af1aa22aa8bd94dbaa0c5ef4e08aa
unknown <729323503@qq.com>
2023-12-04 16:32:10 +0100

    update trainer with get_device

--- trainer.py
+++ trainer.py
@@ -8,6 +8,9 @@ from torch import nn
 from transformers import Trainer, TrainingArguments
 from transformers import DataCollatorForTokenClassification
 
+def get_device():
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 class CustomTrainerA(Trainer):
     def compute_loss(self, 
                      model, 
@@ -24,7 +27,7 @@ class CustomTrainerA(Trainer):
              9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0,
              16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0,
              23.0, 24.0, 25.0, 26.0, 27.0, 28.0, 29.0, 30.0, 31.0], 
-            device=model.device)
+            device=get_device())
         )
         loss = loss_fct(logits.view(-1, 
                                     self.model.config.num_labels), 
@@ -57,7 +60,7 @@ class CustomTrainerB(Trainer):
         loss_fct = nn.CrossEntropyLoss(weight=torch.tensor(
             [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 
              9.0, 10.0, 11.0], 
-            device=model.device)
+            device=get_device())
         )
         loss = loss_fct(logits.view(-1, 
                                     self.model.config.num_labels), 
